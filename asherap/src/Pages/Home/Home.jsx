5c1086069120d83e471c6a735562f5a40cc45269
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-12-03 12:58:54 -0500

    added images with links

--- asherap/src/Pages/Home/Home.jsx
+++ asherap/src/Pages/Home/Home.jsx
@@ -1,17 +1,21 @@
+import Portfolio from '../Portfolio/Portfolio'
 import './home.css'
 
 const Home = () => {
 
   return(
-    <div>
-      <div>
-        <div>
+    <div id="homeContainer">
+      <div id="homeHeader">
+        <div id="headerTitle">
           Anzee Sherap
         </div>
-        <div>
+        <div id="subheader">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         </div>
       </div>
+      <Portfolio />
     </div>
   )
 }
--- asherap/src/Pages/Portfolio/Portfolio.jsx
+++ asherap/src/Pages/Portfolio/Portfolio.jsx
@@ -4,7 +4,38 @@ const Portfolio = () => {
 
   return (
     <div>
-      portfolio placeholder
+      <div id="portfolioContainer">
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+        <div className="portfolioImageContainer">
+          <a href="https://google.com" target="_blank">
+            <img className="portfolioImage" src="https://frictionfilmsnet.files.wordpress.com/2020/09/screenshot-2020-09-25-at-16.56.26.png?w=448" alt="graph" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
